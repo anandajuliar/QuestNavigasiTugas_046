82c40ddda5bdb0b16724cd1ba4356245f5b86f4d
anandajuliar <anandajulia001@gmail.com>
2025-11-09 12:40:57 +0700

    membuat custom card(nnti tsmpilnnya mau gmn)

--- app/src/main/java/com/example/questnavigasitugas_046/view/page2.kt
+++ app/src/main/java/com/example/questnavigasitugas_046/view/page2.kt
@@ -10,9 +10,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -92,4 +95,17 @@ fun Page2(
             }
         }
     }
+}
+
+@Composable
+fun PesertaCard(peserta: Peserta, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
 

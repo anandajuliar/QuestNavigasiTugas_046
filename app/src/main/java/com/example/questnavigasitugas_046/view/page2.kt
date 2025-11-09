9ff2cef73e86c083d44ecd5e0ada6e82365c955b
anandajuliar <anandajulia001@gmail.com>
2025-11-09 12:34:17 +0700

    menambahkan fungsi page 2 dan kolom dasar

--- app/src/main/java/com/example/questnavigasitugas_046/view/page2.kt
+++ app/src/main/java/com/example/questnavigasitugas_046/view/page2.kt
@@ -1,5 +1,12 @@
 package com.example.questnavigasitugas_046.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
 data class Peserta(
     val nama: String,
     val jenisKelamin: String,
@@ -33,4 +40,16 @@ val listPeserta = listOf(
         alamat = "Jogja"
     )
 )
+
+@Composable
+fun Page2(
+    onNavigateToBeranda: () -> Unit,
+    onNavigateToForm: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    )  }
 

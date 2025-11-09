4edb385b5eeb94d34d4d146d77570c501228db8a
anandajuliar <anandajulia001@gmail.com>
2025-11-09 12:31:52 +0700

    menambahkan data class Peserta dan list data statik (yg ditampilkan)

--- app/src/main/java/com/example/questnavigasitugas_046/view/page2.kt
+++ app/src/main/java/com/example/questnavigasitugas_046/view/page2.kt
@@ -1,2 +1,36 @@
 package com.example.questnavigasitugas_046.view
 
+data class Peserta(
+    val nama: String,
+    val jenisKelamin: String,
+    val status: String,
+    val alamat: String
+)
+
+val listPeserta = listOf(
+    Peserta(
+        nama = "Asroni Sukirman",
+        jenisKelamin = "Laki - Laki",
+        status = "Cerai",
+        alamat = "Sleman"
+    ),
+    Peserta(
+        nama = "Aprilia Kurnianti",
+        jenisKelamin = "Perempuan",
+        status = "Lajang",
+        alamat = "Bantul"
+    ),
+    Peserta(
+        nama = "Haris Setyawan",
+        jenisKelamin = "Laki - Laki",
+        status = "Kawin",
+        alamat = "Jogja"
+    ),
+    Peserta(
+        nama = "Dwijoko",
+        jenisKelamin = "Laki - Laki",
+        status = "Kawin",
+        alamat = "Jogja"
+    )
+)
+

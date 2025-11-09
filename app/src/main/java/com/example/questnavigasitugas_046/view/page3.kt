bbf578ad0deb0e93e68e75c3fb8a116210c5820a
anandajuliar <anandajulia001@gmail.com>
2025-11-09 13:04:52 +0700

    layoput utaman

--- app/src/main/java/com/example/questnavigasitugas_046/view/page3.kt
+++ app/src/main/java/com/example/questnavigasitugas_046/view/page3.kt
@@ -1,8 +1,11 @@
 package com.example.questnavigasitugas_046.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -17,4 +20,13 @@ fun Page3(
     var isDropdownExpanded by remember { mutableStateOf(false) }
     val statusOptions = listOf("Lajang", "Kawin", "Cerai")
     var showDialog by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 24.dp, vertical = 16.dp)
+    ) {
     }
+
+
+

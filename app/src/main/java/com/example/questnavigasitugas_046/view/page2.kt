5c42d0af2acbd714d86a9d57d8b752f327a7116e
anandajuliar <anandajulia001@gmail.com>
2025-11-09 12:39:12 +0700

    membuat 2 tombol navigasi

--- app/src/main/java/com/example/questnavigasitugas_046/view/page2.kt
+++ app/src/main/java/com/example/questnavigasitugas_046/view/page2.kt
@@ -2,10 +2,14 @@ package com.example.questnavigasitugas_046.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -72,6 +76,19 @@ fun Page2(
         ) {
             items(listPeserta) { peserta ->
                 PesertaCard(peserta = peserta)
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button(onClick = onNavigateToBeranda) {
+                Text(text = "Beranda")
+            }
+            Button(onClick = onNavigateToForm) {
+                Text(text = "Formulir")
             }
         }
     }

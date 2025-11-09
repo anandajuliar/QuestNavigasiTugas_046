33d8cc1001f6c51f1ce2151f416cb3b1a7a4be31
anandajuliar <anandajulia001@gmail.com>
2025-11-09 12:12:33 +0700

    membuat text ambil dari string

--- app/src/main/java/com/example/questnavigasitugas_046/view/page1.kt
+++ app/src/main/java/com/example/questnavigasitugas_046/view/page1.kt
@@ -5,7 +5,11 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun WelcomeScreen(
@@ -18,4 +22,11 @@ fun WelcomeScreen(
             .padding(16.dp),
         contentAlignment = Alignment.Center
     ) {
+        Text(
+            text = stringResource(R.string.welcome_title),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+    }
     }

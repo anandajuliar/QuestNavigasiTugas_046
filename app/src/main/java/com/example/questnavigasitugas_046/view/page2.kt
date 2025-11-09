caca566588d010c26385b156529483a45a6b4ba7
anandajuliar <anandajulia001@gmail.com>
2025-11-09 12:35:58 +0700

    menambahkan jududl

--- app/src/main/java/com/example/questnavigasitugas_046/view/page2.kt
+++ app/src/main/java/com/example/questnavigasitugas_046/view/page2.kt
@@ -3,9 +3,12 @@ package com.example.questnavigasitugas_046.view
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 data class Peserta(
     val nama: String,
@@ -51,5 +54,11 @@ fun Page2(
         modifier = modifier
             .fillMaxSize()
             .padding(16.dp)
-    )  }
+    ){
+        Text(
+            text = "List Daftar Peserta",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        ) }
 

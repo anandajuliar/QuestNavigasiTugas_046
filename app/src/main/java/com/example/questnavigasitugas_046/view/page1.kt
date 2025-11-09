0f3c4504f95a6ccc928d73aa8c98a8a8c2aae382
anandajuliar <anandajulia001@gmail.com>
2025-11-09 12:07:07 +0700

    menambah logo dan membuat fungsi pada page 1

--- app/src/main/java/com/example/questnavigasitugas_046/view/page1.kt
+++ app/src/main/java/com/example/questnavigasitugas_046/view/page1.kt
@@ -1,2 +1,12 @@
 package com.example.questnavigasitugas_046.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WelcomeScreen(
+    modifier: Modifier = Modifier,
+    onNavigateToPage2: () -> Unit
+) {
+
+    }

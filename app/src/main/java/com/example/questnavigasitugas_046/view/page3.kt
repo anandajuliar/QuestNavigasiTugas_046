13c07ecf790675c0b57665db9aff73a6d8a12167
anandajuliar <anandajulia001@gmail.com>
2025-11-09 13:05:50 +0700

    judul

--- app/src/main/java/com/example/questnavigasitugas_046/view/page3.kt
+++ app/src/main/java/com/example/questnavigasitugas_046/view/page3.kt
@@ -2,10 +2,16 @@ package com.example.questnavigasitugas_046.view
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -26,6 +32,16 @@ fun Page3(
             .fillMaxSize()
             .padding(horizontal = 24.dp, vertical = 16.dp)
     ) {
+        Text(
+            text = "Formulir Pendaftaran",
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp),
+            textAlign = TextAlign.Center
+        )
+    }
     }
 
 

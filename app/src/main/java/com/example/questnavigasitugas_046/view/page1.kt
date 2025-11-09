30400deaab3a3cdadb6064401d513bd67b2fea92
anandajuliar <anandajulia001@gmail.com>
2025-11-09 12:10:42 +0700

    membuat dasar kolom

--- app/src/main/java/com/example/questnavigasitugas_046/view/page1.kt
+++ app/src/main/java/com/example/questnavigasitugas_046/view/page1.kt
@@ -1,12 +1,21 @@
 package com.example.questnavigasitugas_046.view
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun WelcomeScreen(
     modifier: Modifier = Modifier,
     onNavigateToPage2: () -> Unit
 ) {
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
     }
